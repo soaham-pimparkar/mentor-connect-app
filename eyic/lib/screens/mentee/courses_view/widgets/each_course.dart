e7f21ccad9c474d550b2bda8664e519896c07b38
Vedant Barve <vedant.barve@upsidehealth.in>
2023-03-20 22:47:19 +0530

    courses

--- eyic/lib/screens/mentee/courses_view/widgets/each_course.dart
+++ eyic/lib/screens/mentee/courses_view/widgets/each_course.dart
@@ -2,63 +2,96 @@ import 'dart:html';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:eyic/api/models/course_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
-class eachCourse extends StatelessWidget {
-  eachCourse({super.key});
-
-  final List<String> useTitle = [
-    "Youtube video: Free code camp",
-    "introduction to web development",
-    "HTML, CSS AND JAVASCRIPT FOR DEVELOPERS",
-    "Youtube video on web dev",
-    "HTML and CSS blog",
-    "Useful Front-End Boilerplates And Starter Kits blog",
-    "Web development wikipedia"
-  ];
-  final List<String> useDescription = [
-    "Learn full-stack web development in this full course for beginners. First, you will learn the basics of HTML, CSS, and JavaScript. Then, you will learn how to put everything together to create a frontend movie search app. Finally, you will learn how to create a backend API to create movie reviews and connect the frontend to the backend. The backend uses Node.js, Express, and MongoDB.",
-    "This course is designed to start you on a path toward future studies in web development and design, no matter how little experience or technical knowledge you currently have. The web is a very big place, and if you are the typical internet user, you probably visit several websites every day, whether for business, entertainment or education",
-    "In this course, we will learn the basic tools that every web page coder needs to know. We will start from the ground up by learning how to implement modern web pages with HTML and CSS. We will then advance to learning how to code our pages such that its components rearrange and resize themselves automatically based on the size of the user’s screen.	",
-    "Learn JavaScript, HTML, and CSS in this Frontend Web Development course. In this massive course, you will go from no coding experience to having the essential skills of a frontend web developer. You will learn various web development technologies and create a few projects along the way. ",
-    "This blog will cover the basics of html and css",
-    "This blog will help you with front end boilder plates and provide starter kits",
-    "This link will lead you to the wikipedia page for web development"
-  ];
-  final List<String> useLink = [
-    "https://youtu.be/nu_pCVPKzTk",
-    "https://www.coursera.org/learn/web-development",
-    "https://www.coursera.org/learn/html-css-javascript-for-web-developers",
-    "https://youtu.be/zJSY8tbf_ys",
-    "https://webdesign.tutsplus.com/articles/html-css-for-beginners-mega-free-course--cms-93199",
-    "https://youtu.be/zJSY8tbf_ys",
-    "https://webdesign.tutsplus.com/articles/html-css-for-beginners-mega-free-course--cms-93199",
-    "https://www.smashingmagazine.com/2021/06/useful-frontend-boilerplates-starter-kits/",
-    "https://en.wikipedia.org/wiki/Web_development#:~:text=Web%20development%20is%20the%20work%20involved%20in%20developing,web%20applications%2C%20electronic%20businesses%2C%20and%20social%20network%20services.",
-  ];
+class CourseDetailsView extends StatelessWidget {
+  final Map data;
+  const CourseDetailsView({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Web development course kit"),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                data["imageUrl"],
+                height: 200,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            ListTile(
+              title: Text(
+                data["title"],
+              ),
+            ),
+            const Divider(),
+            ListTile(
+              title: Text("Youtube Links"),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: (data["ytLinks"] as List).length,
+              itemBuilder: (context, index) {
+                final ytData = (data["ytLinks"] as List)[index];
+                return ListTile(
+                  onTap: () => launchUrlString(ytData["link"]),
+                  title: Text(
+                    ytData["title"],
+                  ),
+                  subtitle: Text(
+                    ytData["authorName"],
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                );
+              },
+            ),
+            const Divider(),
+            ListTile(
+              title: Text("Blogs"),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: (data["blogs"] as List).length,
+              itemBuilder: (context, index) {
+                final blog = (data["blogs"] as List)[index];
+                return ListTile(
+                  onTap: () => launchUrlString(blog["link"]),
+                  title: Text(
+                    blog["title"],
+                  ),
+                  subtitle: Text(
+                    blog["authorName"],
+                  ),
+                  trailing: const Icon(Icons.arrow_forward),
+                );
+              },
+            ),
+            const Divider(),
+            ListTile(
+              title: Text("Resources"),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: (data["resources"] as List).length,
+              itemBuilder: (context, index) {
+                final resource = (data["resources"] as List)[index];
+                return ListTile(
+                  onTap: () => launchUrlString(resource["link"]),
+                  title: Text(
+                    resource["title"],
+                  ),
+                  trailing: const Icon(Icons.arrow_forward),
+                );
+              },
+            ),
+          ],
         ),
-        body: ListView.builder(
-            //padding:
-            //  const EdgeInsets.symmetric(horizontal: 7, vertical: 7),
-            itemCount: useTitle.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                contentPadding:
-                    EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                title: Text(useTitle[index]),
-                subtitle: Text(useDescription[index]),
-                leading: ElevatedButton(
-                    onPressed: () {
-                      _launchInBrowser(Uri.parse(useLink[index]));
-                    },
-                    child: Icon(Icons.link)),
-              );
-            }));
+      ),
+    );
   }
 }
 
